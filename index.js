935044137c7e6b5f8b3ac25517f5caa1c62a44ba
Cedric Patton <ceddyp117@gmail.com>
2020-06-09 19:32:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function appendKitten(){
 
 function prependKitten(){
   kittens = ['Milo', 'Otis', 'Garfield'];
-  return 'Arnold'.concat(kittens);
+  return ('Arnold').concat(kittens);
 }
 
 function removeLastKitten(){
