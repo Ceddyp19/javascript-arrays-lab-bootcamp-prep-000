2b76bacce875af022d160e19e0c70b69d9692dca
Cedric Patton <ceddyp117@gmail.com>
2020-06-09 19:17:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function destructivelyRemoveFirstKitten(){
 
 function appendKitten(){
   kittens = ['Milo', 'Otis', 'Garfield'];
-  return [kittens,...'Broom'];
+  return 
 }
 
 function prependKitten(){
