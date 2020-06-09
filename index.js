e454d5e13d7c49e369bed341f654436efac243e6
Cedric Patton <ceddyp117@gmail.com>
2020-06-09 18:52:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -38,6 +38,6 @@ function removeLastKitten(){
 
 function removeFirstKitten(){
    kittens = ['Milo', 'Otis', 'Garfield'];
-   kittens.slice(1);
+   kittens = kittens.slice(1);
    return kittens;
 }
